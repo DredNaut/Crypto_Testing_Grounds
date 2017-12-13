24bf1bbc35cc79b80895effd8d244288f206baf3
Jared Knutson <jknutson2494@gmail.com>
2017-12-12 23:03:50 -0800

    Fixed

--- coinMarketCapAPI.py
+++ coinMarketCapAPI.py
@@ -12,17 +12,14 @@ from time import strftime
 # Modify this function to allow for all query options from this web API
 # For an example of all arguments visit https://coinmarketcap.com/api/
 # Create exception handling for command line arguments
-def coinMarketCap_search(query,flag):
+def coinMarketCap_search(query):
     url = 'https://api.coinmarketcap.com/v1/ticker/?limit='
     limit = query
     final_url = url + limit
     json_obj = urllib2.urlopen(final_url)
     data = json.load(json_obj)
-    filename = "CMCtop" + strftime("%m%Y%d-%H") + ".csv"
-    if(flag == 'r'):
-        ledger = open(filename, 'w')
-    else:
-        ledger = open(filename, 'a')
+    filename = "/home/drednaut/Programming/Python/Bittrex/Crypto_Testing_Grounds/CMCdata/CMCtop" + strftime("%m%Y%d-%H") + ".csv"
+    ledger = open(filename, 'w')
 
     for item in data:
         string = item['name'] + "," + item['price_usd'] + "," + item['market_cap_usd'] + "," + item['24h_volume_usd'] + "," + item['last_updated'] + "\n"
@@ -33,5 +30,4 @@ def coinMarketCap_search(query,flag):
 
 
 limit = sys.argv[1]
-write_flag = sys.argv[2]
-coinMarketCap_search(limit,write_flag)
+coinMarketCap_search(limit)
